da5b0af37051615036af1cea301615225a1d073c
Danil Semelenov <mail@danil.mobi>
2018-06-06 23:11:38 +0300

    Resolve a module entry path from a module name

--- index.js
+++ index.js
@@ -7,33 +7,44 @@ const nodeResolve = require('rollup-plugin-node-resolve');
 const path = require('path');
 const rollup = require('rollup');
 
-async function bundleModule(input) {
-  input = '../node_modules/hyperscript/index.js';
+const rollupPlugins = [
+  commonJS(),
+  nodeBuiltins(),
+  nodeGlobals(),
+  nodeResolve({ jsnext: true }),
+];
+
+async function bundleModule(modulePath) {
+  const entryPath = await resolveModuleEntry(modulePath);
   const bundle = await rollup.rollup({
-    input,
-    plugins: [
-      commonJS(),
-      nodeBuiltins(),
-      nodeGlobals(),
-      nodeResolve({ jsnext: true }),
-    ],
+    input: entryPath,
+    plugins: rollupPlugins,
   });
-  const { code } = await bundle.generate({ format: 'es' });
-  console.log(code);
+  const outputPath = path.join(
+    path.dirname(modulePath),
+    '../es_modules',
+    `${path.basename(modulePath)}.js`,
+  );
+  await bundle.write({ file: outputPath, format: 'es' });
+}
+
+function resolveModuleEntry(modulePath) {
+  return rollupPlugins
+    .find(plugin => plugin.name === 'node-resolve')
+    .resolveId(path.basename(modulePath), path.dirname(modulePath));
 }
 
 module.exports = async function sync() {
   const prefix = await findNPMPrefix(process.cwd());
   const modulesPath = path.join(prefix, 'node_modules');
   if (fs.existsSync(modulesPath)) {
-    for (const filename of fs.readdirSync(modulesPath)) {
-      if (
-        fs.statSync(path.join(modulesPath, filename)).isDirectory() &&
-        !filename.startsWith('.')
-      ) {
-        await bundleModule(filename);
-        process.exit();
-      }
+    //    for (const name of fs.readdirSync(modulesPath)) {
+    const name = 'hyperscript';
+    const modulePath = path.join(modulesPath, name);
+    if (fs.statSync(modulePath).isDirectory() && !name.startsWith('.')) {
+      await bundleModule(modulePath);
+      process.exit();
     }
+    //    }
   }
 };
